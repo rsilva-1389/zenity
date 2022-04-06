e28282a7525d307e15a93b10ace2332fd2956d54
Renan Farias Silva <rsilva.1389@gmail.com>
2022-04-06 20:35:32 -0300

    First commit - Sprint initalizr and gitignore

---
+++ src/main/java/br/com/zup/zenity/ZenityApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.zenity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/zenity/ZenityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.zenity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
